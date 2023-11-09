a0287650ccd752f9ff88ef7b09a28e47012d1e4a
Hengly Aun <aunhengly@gmail.com>
2023-11-09 03:17:21 -0800

    update the alcohol in put and dockerfile is working and tested

--- app.py
+++ app.py
@@ -29,4 +29,5 @@ def predict():
     return render_template('index.html', predict=pred[0], predictions=predictions)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
+    # app.run(debug=True)
